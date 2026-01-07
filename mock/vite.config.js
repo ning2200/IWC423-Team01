35acbcd656ec6d3e16e7c2d603e7a31600a30b62
Gabriel Ong Zhe Mian <angryapplegravy@gmail.com>
2026-01-07 15:02:09 +0800

    feat(app): added

--- mock/vite.config.js
+++ mock/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/IWC423-Team01/',
+  base: '/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
